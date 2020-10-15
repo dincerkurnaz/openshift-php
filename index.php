4b6c16420448e6eb82305bd17d601bf116051196
dinçer <dincersalih79@gmail.com>
2020-10-15 10:43:43 +0300

    Add files via upload
    ilk file

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+
+<head>
+<title>Hello World!</title>
+</head>
+
+<body>
+
+<?php echo "Dinçer ve Openshift :P"; ?>
+<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?><br /><br />
+
+<?php $links = [];
+  foreach($_ENV as $key => $value) {
+    if(preg_match("/^(.*)_PORT_([0-9]*)_(TCP|UDP)$/", $key, $matches)) {
+      $links[] = [
+        "name"  => $matches[1],
+        "port"  => $matches[2],
+        "proto" => $matches[3],
+        "value" => $value
+      ];
+    }
+  }
+
+  if($links) {
+    foreach($links as $link) {
+      echo $link["name"]; ?>  listening on port <?php echo $link["port"]."/".$link["proto"]; ?> available at <?php echo $link["value"]; ?><br /><?php
+    }
+  }
+
+}
+?>
+
+</body>
+</html>
